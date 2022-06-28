85de4a0641c9af1797650058401ba6dca2925650
Azu0925 <yamaneko564783@gmail.com>
2022-06-29 02:18:46 +0900

    create new task page

---
+++ src/pages/NewTask.jsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+import { url } from "../const";
+import { Header } from "../components/Header";
+import "./newTask.css"
+import { useHistory } from "react-router-dom";
+
+export const NewTask = () => {
+  const [selectListId, setSelectListId] = useState();
+  const [lists, setLists] = useState([]);
+  const [title, setTitle] = useState("");
+  const [detail, setDetail] = useState("");
+  const [cookies] = useCookies();
+  const history = useHistory();
+  const handleTitleChange = (e) => setTitle(e.target.value);
+  const handleDetailChange = (e) => setDetail(e.target.value);
+  const handleSelectList = (id) => setSelectListId(id);
+  const onCreateTask = () => {
+    const data = {
+      title: title,
+      detail: detail,
+      done: false,
+    };
+
+    axios.post(`${url}/lists/${selectListId}/tasks`, data, {
+        headers: {
+          authorization: `Bearer ${cookies.token}`
+        }
+    })
+    .then(() => {
+      history.push("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  useEffect(() => {
+    axios.get(`${url}/lists`, {
+      headers: {
+        authorization: `Bearer ${cookies.token}`
+      }
+    })
+    .then((res) => {
+      setLists(res.data)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }, [])
+
+  return (
+    <div>
+      <Header />
+      <main className="new-task">
+        <h2>タスク新規作成</h2>
+        <form className="new-task-form">
+          <label>リスト</label><br />
+          <select onChange={(e) => handleSelectList(e.target.value)} className="new-task-select-list">
+            {lists.map((list, key) => (
+              <option key={key} className="list-item" value={list.id}>{list.title}</option>
+            ))}
+          </select><br />
+          <label>タイトル</label><br />
+          <input type="text" onChange={handleTitleChange} className="new-task-title" /><br />
+          <label>詳細</label><br />
+          <textarea type="text" onChange={handleDetailChange} className="new-task-detail" /><br />
+          <button type="button" className="new-task-button" onClick={onCreateTask}>作成</button>
+        </form>
+      </main>
+    </div>
+  )
+}
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { NotFound } from "../pages/NotFound";
 import { SignIn } from "../pages/SignIn";
+import { NewTask } from "../pages/NewTask";
 
 export const Router = () => {
   const auth = useSelector((state) => state.auth.isSignIn)
@@ -11,9 +12,12 @@ export const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/signin' component={SignIn} />
+        <Route exact path="/signin" component={SignIn} />
         {auth ? (
-          <Route exact path='/' component={Home} />
+          <>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/task/new" component={NewTask} />
+          </>
         ) : (
           <Redirect to="/signin" />
         )}
