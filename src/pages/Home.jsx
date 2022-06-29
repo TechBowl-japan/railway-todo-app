7cbfd4e65671ec0da24735b495b4d0bf2c9036cb
Azu0925 <yamaneko564783@gmail.com>
2022-06-30 00:53:03 +0900

    task list display switching

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,13 @@ import "./home.css";
 
 
 export const Home = () => {
+  const [isDoneDisplay, setIsDoneDisplay] = useState("todo"); // todo->未完了 done->完了
   const [lists, setLists] = useState([]);
   const [selectListId, setSelectListId] = useState();
   const [tasks, setTasks] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
   const [cookies] = useCookies();
+  const handleIsDoneDisplayChange = (e) => setIsDoneDisplay(e.target.value);
   useEffect(() => {
     axios.get(`${url}/lists`, {
       headers: {
@@ -65,9 +67,12 @@ export const Home = () => {
       <main className="taskList">
         <p className="error-message">{errorMessage}</p>
         <div>
-          <div className="list-menu">
-            <p><Link to="/list/new">リスト新規作成</Link></p>
-            <p><Link to={`/lists/${selectListId}/edit`}>選択中のリストを編集</Link></p>
+          <div className="list-header">
+            <h2>リスト一覧</h2>
+            <div className="list-menu">
+              <p><Link to="/list/new">リスト新規作成</Link></p>
+              <p><Link to={`/lists/${selectListId}/edit`}>選択中のリストを編集</Link></p>
+            </div>
           </div>
           <ul className="list-tab">
             {lists.map((list, key) => {
@@ -88,7 +93,13 @@ export const Home = () => {
               <h2>タスク一覧</h2>
               <Link to="/task/new">タスク新規作成</Link>
             </div>
-            <Tasks tasks={tasks} selectListId={selectListId} />
+            <div className="display-select-wrapper">
+              <select onChange={handleIsDoneDisplayChange} className="display-select">
+                <option value="todo">未完了</option>
+                <option value="done">完了</option>
+              </select>
+            </div>
+            <Tasks tasks={tasks} selectListId={selectListId} isDoneDisplay={isDoneDisplay} />
           </div>
         </div>
       </main>
@@ -96,13 +107,35 @@ export const Home = () => {
   )
 }
 
+// 表示するタスク
 const Tasks = (props) => {
-  const { tasks, selectListId } = props;
+  const { tasks, selectListId, isDoneDisplay } = props;
   if (tasks === null) return <></>
+
+  if(isDoneDisplay == "done"){
+    return (
+      <ul>
+        {tasks.filter((task) => {
+          return task.done === true
+        })
+        .map((task, key) => (
+          <li key={key} className="task-item">
+            <Link to={`/lists/${selectListId}/tasks/${task.id}`} className="task-item-link">
+              {task.title}<br />
+              {task.done ? "完了" : "未完了"}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    )
+  }
 
   return (
     <ul>
-      {tasks.map((task, key) => (
+      {tasks.filter((task) => {
+        return task.done === false
+      })
+      .map((task, key) => (
         <li key={key} className="task-item">
           <Link to={`/lists/${selectListId}/tasks/${task.id}`} className="task-item-link">
             {task.title}<br />
