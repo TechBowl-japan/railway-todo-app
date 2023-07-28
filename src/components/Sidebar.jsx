620420754d3b3ba9d3db161f4e4045b665bdd42e
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2023-07-28 13:25:39 +0900

    update: add sidebar states

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,39 +20,59 @@ export const Sidebar = () => {
   ]
 
   const activeId = '1'
+  const isLoggedIn = true
 
   return (
     <div className={styles.sidebar}>
       <h1 className={styles.sidebar__title}>Todos</h1>
-      <div className={styles.sidebar__lists}>
-        <h2 className={styles.sidebar__lists_title}>Lists</h2>
-        <ul className={styles.sidebar__lists_items}>
-          {lists.map((listItem) => (
-            <li key={listItem.id}>
-              <Link
-                data-active={listItem.id === activeId}
-                to={`/lists/${listItem.id}`}
-                className={styles.sidebar__lists_item}
-              >
-                <ListIcon aria-hidden className={styles.sidebar__lists_icon} />
-                {listItem.name}
-              </Link>
-            </li>
-          ))}
-          <li>
-            <button type="button" className={styles.sidebar__lists_button}>
-              <PlusIcon className={styles.sidebar__lists_plus_icon} />
-              New List...
+      {isLoggedIn ? (
+        <>
+          {lists && (
+            <div className={styles.sidebar__lists}>
+              <h2 className={styles.sidebar__lists_title}>Lists</h2>
+              <ul className={styles.sidebar__lists_items}>
+                {lists.map((listItem) => (
+                  <li key={listItem.id}>
+                    <Link
+                      data-active={listItem.id === activeId}
+                      to={`/lists/${listItem.id}`}
+                      className={styles.sidebar__lists_item}
+                    >
+                      <ListIcon
+                        aria-hidden
+                        className={styles.sidebar__lists_icon}
+                      />
+                      {listItem.name}
+                    </Link>
+                  </li>
+                ))}
+                <li>
+                  <button
+                    type="button"
+                    className={styles.sidebar__lists_button}
+                  >
+                    <PlusIcon className={styles.sidebar__lists_plus_icon} />
+                    New List...
+                  </button>
+                </li>
+              </ul>
+            </div>
+          )}
+          <div className={styles.sidebar__spacer} aria-hidden />
+          <div className={styles.sidebar__account}>
+            <p className={styles.sidebar__account_name}>John Doe</p>
+            <button type="button" className={styles.sidebar__account_logout}>
+              Logout
             </button>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.sidebar__account}>
-        <p className={styles.sidebar__account_name}>John Doe</p>
-        <button type="button" className={styles.sidebar__account_logout}>
-          Logout
-        </button>
-      </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <Link to="/login" className={styles.sidebar__login}>
+            Login
+          </Link>
+        </>
+      )}
     </div>
   )
 }
