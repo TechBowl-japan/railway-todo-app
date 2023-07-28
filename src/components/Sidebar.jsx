5cd06c8a3370ac31139c0a624ad7e64e4314a90a
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2023-07-28 13:13:26 +0900

    add: sidebar mock

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,58 @@
+import { ListIcon } from '~/icons/ListIcon'
+import styles from './Sidebar.module.css'
+import { Link } from 'react-router-dom'
+import { PlusIcon } from '~/icons/PlusIcon'
+
+export const Sidebar = () => {
+  const lists = [
+    {
+      id: '1',
+      name: 'List 1',
+    },
+    {
+      id: '2',
+      name: 'List 2',
+    },
+    {
+      id: '3',
+      name: 'List 3',
+    },
+  ]
+
+  const activeId = '1'
+
+  return (
+    <div className={styles.sidebar}>
+      <h1 className={styles.sidebar__title}>Todos</h1>
+      <div className={styles.sidebar__lists}>
+        <h2 className={styles.sidebar__lists_title}>Lists</h2>
+        <ul className={styles.sidebar__lists_items}>
+          {lists.map((listItem) => (
+            <li key={listItem.id}>
+              <Link
+                data-active={listItem.id === activeId}
+                to={`/lists/${listItem.id}`}
+                className={styles.sidebar__lists_item}
+              >
+                <ListIcon aria-hidden className={styles.sidebar__lists_icon} />
+                {listItem.name}
+              </Link>
+            </li>
+          ))}
+          <li>
+            <button type="button" className={styles.sidebar__lists_button}>
+              <PlusIcon className={styles.sidebar__lists_plus_icon} />
+              New List...
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.sidebar__account}>
+        <p className={styles.sidebar__account_name}>John Doe</p>
+        <button type="button" className={styles.sidebar__account_logout}>
+          Logout
+        </button>
+      </div>
+    </div>
+  )
+}
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -9,32 +9,36 @@ import { NewList } from '../pages/NewList'
 import { EditTask } from '../pages/EditTask'
 import { SignUp } from '../pages/SignUp'
 import { EditList } from '../pages/EditList'
+import { Sidebar } from '~/components/Sidebar'
 
 export const Router = () => {
-  const auth = useSelector(state => state.auth.token !== null)
+  const auth = useSelector((state) => state.auth.token !== null)
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route exact path="/signin" element={<SignIn />} />
-        <Route exact path="/signup" element={<SignUp />} />
-        {auth ? (
-          <>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/task/new" element={<NewTask />} />
-            <Route exact path="/list/new" element={<NewList />} />
-            <Route
-              exact
-              path="/lists/:listId/tasks/:taskId"
-              element={<EditTask />}
-            />
-            <Route exact path="/lists/:listId/edit" element={<EditList />} />
-          </>
-        ) : (
-          <Route path="/" element={<Navigate to="/signin" />} />
-        )}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Sidebar />
+      <div className="main_content">
+        <Routes>
+          <Route exact path="/signin" element={<SignIn />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          {auth ? (
+            <>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/task/new" element={<NewTask />} />
+              <Route exact path="/list/new" element={<NewList />} />
+              <Route
+                exact
+                path="/lists/:listId/tasks/:taskId"
+                element={<EditTask />}
+              />
+              <Route exact path="/lists/:listId/edit" element={<EditList />} />
+            </>
+          ) : (
+            <Route path="/" element={<Navigate to="/signin" />} />
+          )}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   )
 }
