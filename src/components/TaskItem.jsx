0515edcd1191bd89f350f390a0a4547e0f636e36
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2023-07-28 22:53:28 +0900

    add: actions stub

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,23 +1,54 @@
+import { Link, useParams } from 'react-router-dom'
 import styles from './TaskItem.module.css'
+import { PencilIcon } from '~/icons/PencilIcon'
+import { TrashIcon } from '~/icons/TrashIcon'
+import { CheckIcon } from '~/icons/CheckIcon'
+import { useCallback } from 'react'
 
 export const TaskItem = ({ task }) => {
+  const { listId } = useParams()
   const { id, title, detail, done } = task
+
+  const handleToggle = useCallback(() => {}, [])
+  const handleDelete = useCallback(() => {}, [])
 
   return (
     <div className={styles.task_item}>
       <div className={styles.task_item__title_container}>
-        {done ? (
-          <div
-            className={styles.task_item__mark____complete}
-            aria-label="Completed"
-          ></div>
-        ) : (
-          <div
-            className={styles.task_item__mark____incomplete}
-            aria-label="Incomplete"
-          ></div>
-        )}
+        <button
+          type="button"
+          onClick={handleToggle}
+          className={styles.task__item__mark_button}
+        >
+          {done ? (
+            <div
+              className={styles.task_item__mark____complete}
+              aria-label="Completed"
+            >
+              <CheckIcon className={styles.task_item__mark____complete_check} />
+            </div>
+          ) : (
+            <div
+              className={styles.task_item__mark____incomplete}
+              aria-label="Incomplete"
+            ></div>
+          )}
+        </button>
         <div className={styles.task_item__title}>{title}</div>
+        <div aria-hidden className={styles.task_item__title_spacer}></div>
+        <Link
+          to={`/lists/${listId}/tasks/${id}`}
+          className={styles.task_item__title_action}
+        >
+          <PencilIcon aria-label="Edit" />
+        </Link>
+        <button
+          onClick={handleDelete}
+          type="button"
+          className={styles.task_item__title_action}
+        >
+          <TrashIcon aria-label="Delete" />
+        </button>
       </div>
       <div className={styles.task_item__detail}>{detail}</div>
     </div>
