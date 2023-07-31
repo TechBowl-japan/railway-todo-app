49c048223f3ce0bc196b97d939585c7454fc3f7c
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2023-07-31 12:00:10 +0900

    add: task edit

--- src/pages/lists/[listId]/tasks/[taskId]/index.page.jsx
+++ src/pages/lists/[listId]/tasks/[taskId]/index.page.jsx
@@ -1,123 +1,148 @@
-import React, { useEffect, useState } from 'react'
-import axios from '~/vendor/axios'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useId, useCallback, useState, useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { BackButton } from '~/components/BackButton'
 import './index.css'
+import { setCurrentList } from '~/store/list'
+import { fetchTodos, updateTodo, deleteTodo } from '~/store/todo'
 
 const EditTask = () => {
-  const navigate = useNavigate()
+  const id = useId()
+
   const { listId, taskId } = useParams()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
   const [title, setTitle] = useState('')
   const [detail, setDetail] = useState('')
-  const [isDone, setIsDone] = useState()
+  const [done, setDone] = useState(false)
+
   const [errorMessage, setErrorMessage] = useState('')
-  const handleTitleChange = e => setTitle(e.target.value)
-  const handleDetailChange = e => setDetail(e.target.value)
-  const handleIsDoneChange = e => setIsDone(e.target.value === 'done')
-  const onUpdateTask = () => {
-    console.log(isDone)
-    const data = {
-      title: title,
-      detail: detail,
-      done: isDone,
-    }
+  const [isSubmitting, setIsSubmitting] = useState(false)
 
-    axios
-      .put(`/lists/${listId}/tasks/${taskId}`, data)
-      .then(res => {
-        console.log(res.data)
-        navigate('/')
-      })
-      .catch(err => {
-        setErrorMessage(`更新に失敗しました。${err}`)
-      })
-  }
-
-  const onDeleteTask = () => {
-    axios
-      .delete(`/lists/${listId}/tasks/${taskId}`)
-      .then(() => {
-        navigate('/')
-      })
-      .catch(err => {
-        setErrorMessage(`削除に失敗しました。${err}`)
-      })
-  }
+  const todo = useSelector(state =>
+    state.todo.todos?.find(todo => todo.id === taskId),
+  )
 
   useEffect(() => {
-    axios
-      .get(`/lists/${listId}/tasks/${taskId}`)
-      .then(res => {
-        const task = res.data
-        setTitle(task.title)
-        setDetail(task.detail)
-        setIsDone(task.done)
+    if (todo) {
+      setTitle(todo.title)
+      setDetail(todo.detail)
+      setDone(todo.done)
+    }
+  }, [todo])
+
+  useEffect(() => {
+    void dispatch(setCurrentList(listId))
+    void dispatch(fetchTodos())
+  }, [listId])
+
+  const onSubmit = useCallback(
+    event => {
+      event.preventDefault()
+
+      setIsSubmitting(true)
+
+      void dispatch(updateTodo({ id: taskId, title, detail, done }))
+        .unwrap()
+        .then(() => {
+          navigate(`/lists/${listId}`)
+        })
+        .catch(err => {
+          setErrorMessage(err.message)
+        })
+        .finally(() => {
+          setIsSubmitting(false)
+        })
+    },
+    [title, taskId, listId, detail, done],
+  )
+
+  const handleDelete = useCallback(() => {
+    if (!window.confirm('Are you sure you want to delete this task?')) {
+      return
+    }
+
+    setIsSubmitting(true)
+
+    void dispatch(deleteTodo({ id: taskId }))
+      .unwrap()
+      .then(() => {
+        navigate(`/`)
       })
       .catch(err => {
-        setErrorMessage(`タスク情報の取得に失敗しました。${err}`)
+        setErrorMessage(err.message)
       })
-  }, [])
+      .finally(() => {
+        setIsSubmitting(false)
+      })
+  }, [taskId])
 
   return (
-    <div>
-      <main className="edit-task">
-        <h2>タスク編集</h2>
-        <p className="error-message">{errorMessage}</p>
-        <form className="edit-task-form">
-          <label>タイトル</label>
-          <br />
+    <main className="edit_list">
+      <BackButton />
+      <h2 className="edit_list__title">Edit List</h2>
+      <p className="edit_list__error">{errorMessage}</p>
+      <form className="edit_list__form" onSubmit={onSubmit}>
+        <fieldset className="edit_list__form_field">
+          <label htmlFor={`${id}-title`} className="edit_list__form_label">
+            Title
+          </label>
           <input
-            type="text"
-            onChange={handleTitleChange}
-            className="edit-task-title"
+            id={`${id}-title`}
+            className="app_input"
+            placeholder="Buy some milk"
             value={title}
+            onChange={event => setTitle(event.target.value)}
           />
-          <br />
-          <label>詳細</label>
-          <br />
+        </fieldset>
+        <fieldset className="edit_list__form_field">
+          <label
+            htmlFor={`${id}-description`}
+            className="edit_list__form_label"
+          >
+            Description
+          </label>
           <textarea
-            type="text"
-            onChange={handleDetailChange}
-            className="edit-task-detail"
+            id={`${id}-description`}
+            className="app_input"
+            placeholder="Blah blah blah"
             value={detail}
+            onChange={event => setDetail(event.target.value)}
           />
-          <br />
+        </fieldset>
+        <fieldset className="edit_list__form_field">
+          <label htmlFor={`${id}-done`} className="edit_list__form_label">
+            Is Done
+          </label>
           <div>
             <input
-              type="radio"
-              id="todo"
-              name="status"
-              value="todo"
-              onChange={handleIsDoneChange}
-              checked={isDone === false ? 'checked' : ''}
+              id={`${id}-done`}
+              type="checkbox"
+              checked={done}
+              onChange={event => setDone(event.target.checked)}
             />
-            未完了
-            <input
-              type="radio"
-              id="done"
-              name="status"
-              value="done"
-              onChange={handleIsDoneChange}
-              checked={isDone === true ? 'checked' : ''}
-            />
-            完了
           </div>
+        </fieldset>
+        <div className="edit_list__form_actions">
+          <Link to="/" data-variant="secondary" className="app_button">
+            Cancel
+          </Link>
+          <div className="edit_list__form_actions_spacer"></div>
           <button
             type="button"
-            className="delete-task-button"
-            onClick={onDeleteTask}
+            className="app_button edit_list__form_actions_delete"
+            disabled={isSubmitting}
+            onClick={handleDelete}
           >
-            削除
+            Delete
           </button>
-          <button
-            type="button"
-            className="edit-task-button"
-            onClick={onUpdateTask}
-          >
-            更新
+          <button type="submit" className="app_button" disabled={isSubmitting}>
+            Update
           </button>
-        </form>
-      </main>
-    </div>
+        </div>
+      </form>
+    </main>
   )
 }
 
