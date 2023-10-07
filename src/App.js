5c86da020f08b146597a5302b8d2bfb9d182ca2e
Aoyama Kazuki <sekibun3109@gmail.com>
2023-10-07 15:13:37 +0900

    reactのバージョンアップ ローカルで起動するように実装

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import { Router } from './routes/Router';
+import "./App.css";
+import { Router } from "./routes/Router";
 
 function App() {
   return (
     <div className="App">
-        <Router />
+      <Router />
     </div>
   );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,14 +2,14 @@ import axios from "axios";
 import React, { useState } from "react";
 import { useCookies } from "react-cookie";
 import { useSelector, useDispatch } from "react-redux";
-import { useHistory, Redirect } from "react-router-dom";
+import { useNavigate, Navigate, Route } from "react-router-dom";
 import { signIn } from "../authSlice";
 import { Header } from "../components/Header";
 import { url } from "../const";
 import "./signUp.css";
 
 export const SignUp = () => {
-  const history = useHistory();
+  const navigation = useNavigate();
   const auth = useSelector((state) => state.auth.isSignIn);
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
@@ -24,22 +24,23 @@ export const SignUp = () => {
     const data = {
       email: email,
       name: name,
-      password: password
+      password: password,
     };
 
-    axios.post(`${url}/users`, data)
+    axios
+      .post(`${url}/users`, data)
       .then((res) => {
         const token = res.data.token;
         dispatch(signIn());
         setCookie("token", token);
-        history.push("/");
+        navigation("/");
       })
       .catch((err) => {
         setErrorMessge(`サインアップに失敗しました。 ${err}`);
-      })
+      });
 
-      if(auth) return <Redirect to="/" />
-  }
+    if (auth) return <Route path="/" element={<Navigate to="/" />} />;
+  };
   return (
     <div>
       <Header />
@@ -47,15 +48,35 @@ export const SignUp = () => {
         <h2>新規作成</h2>
         <p className="error-message">{errorMessage}</p>
         <form className="signup-form">
-          <label>メールアドレス</label><br />
-          <input type="email" onChange={handleEmailChange} className="email-input" /><br />
-          <label>ユーザ名</label><br />
-          <input type="text" onChange={handleNameChange} className="name-input" /><br />
-          <label>パスワード</label><br />
-          <input type="password" onChange={handlePasswordChange} className="password-input" /><br />
-          <button type="button" onClick={onSignUp} className="signup-button">作成</button>
+          <label>メールアドレス</label>
+          <br />
+          <input
+            type="email"
+            onChange={handleEmailChange}
+            className="email-input"
+          />
+          <br />
+          <label>ユーザ名</label>
+          <br />
+          <input
+            type="text"
+            onChange={handleNameChange}
+            className="name-input"
+          />
+          <br />
+          <label>パスワード</label>
+          <br />
+          <input
+            type="password"
+            onChange={handlePasswordChange}
+            className="password-input"
+          />
+          <br />
+          <button type="button" onClick={onSignUp} className="signup-button">
+            作成
+          </button>
         </form>
       </main>
     </div>
-  )
-}
+  );
+};
