03a260bacda038743fe104b114424a2baf04ea69
Yuto0508 <yuto4139@outlook.jp>
2023-11-22 16:26:33 +0900

    main

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,13 @@ module.exports = {
     "plugin:prettier/recommended", // Prettierとの連携
   ],
   parserOptions: {
-    ecmaVersion: 13,
+    ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["prettier"],
+  plugins: ["prettier", "react-hooks"], // プラグインの追加
   rules: {
     "prettier/prettier": "error", // Prettierのルールをエラーとして扱う
+    "react-hooks/rules-of-hooks": "error", // react-hooksのルールを追加
+    "react-hooks/exhaustive-deps": "warn", // react-hooksのルールを追加
   },
 };
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 
 ReactDOM.render(
-  <Provider store={store}>
+ <Provider store={store}>
     <CookiesProvider>
       <App />
     </CookiesProvider>
