59eb777d45165bbc2558b19df9b629db1a8b6807
seigetsu <nyarlathotep44@gmail.com>
2023-11-24 23:43:52 +0900

    ライブラリのアップデートと移行

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useCookies } from "react-cookie";
 import { useSelector, useDispatch } from "react-redux/es/exports";
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import  { signOut } from "../authSlice";
 import "./header.css";
 
 export const Header = () => {
   const auth = useSelector((state) => state.auth.isSignIn)
   const dispatch = useDispatch();
-  const history = useHistory();
+  const history = useNavigate();
   const [cookies, setCookie, removeCookie] = useCookies();
   const handleSignOut = () => {
     dispatch(signOut());
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { NotFound } from "../pages/NotFound";
 import { SignIn } from "../pages/SignIn";
@@ -15,22 +15,26 @@ export const Router = () => {
 
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/signup" component={SignUp} />
-        {auth ? (
-          <>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/task/new" component={NewTask} />
-            <Route exact path="/list/new" component={NewList} />
-            <Route exact path="/lists/:listId/tasks/:taskId" component={EditTask} />
-            <Route exact path="/lists/:listId/edit" component={EditList} />
-          </>
-        ) : (
-          <Redirect to="/signin" />
-        )}
-        <Route component={NotFound} />
-      </Switch>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={
+          auth ? <Home /> : <Navigate replace to="/signin"/>
+        } />
+        <Route path="/task/new" element={
+          auth ? <NewTask /> : <Navigate replace to="/signin"/>
+        } />
+        <Route path="/list/new" element={
+          auth ? <NewList /> : <Navigate replace to="/signin"/>
+        } />
+        <Route path="/lists/:listId/tasks/:taskId" element={
+          auth ? <EditTask /> : <Navigate replace to="/signin"/>
+        } />
+        <Route path="/lists/:listId/edit" element={
+          auth ? <EditList /> : <Navigate replace to="/signin"/>
+        } />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
   )
 }
