1179c6316229553833ec8e4c37f0a30debab2f38
yusaku-nakada-satt <y_nakada@satt.jp>
2024-02-27 13:30:59 +0900

    prettier導入

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
     <CookiesProvider>
       <App />
     </CookiesProvider>
-  </Provider>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
