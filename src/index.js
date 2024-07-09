7504dc666cc985c72a0342809327c4fc83a91219
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-07-09 22:39:39 +0900

    createRoot

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CookiesProvider } from "react-cookie";
 import { Provider } from "react-redux";
 import { store } from "./store"
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <CookiesProvider>
       <App />
     </CookiesProvider>
   </Provider>,
-  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
