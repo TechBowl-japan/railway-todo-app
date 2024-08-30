2fe02d71a1c953455fb750ad7d01b83720032461
yukinko05 <grey.sora.35@gmail.com>
2024-08-30 22:13:26 +0900

    Routeコンポーネントを修正

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Navigate, BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { NotFound } from "../pages/NotFound";
 import { SignIn } from "../pages/SignIn";
@@ -12,28 +12,23 @@ import { EditList } from "../pages/EditList";
 
 export const Router = () => {
   const auth = useSelector((state) => state.auth.isSignIn);
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/signup" component={SignUp} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
         {auth ? (
           <>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/task/new" component={NewTask} />
-            <Route exact path="/list/new" component={NewList} />
-            <Route
-              exact
-              path="/lists/:listId/tasks/:taskId"
-              component={EditTask}
-            />
-            <Route exact path="/lists/:listId/edit" component={EditList} />
+            <Route path="/" element={<Home />} />
+            <Route path="/task/new" element={<NewTask />} />
+            <Route path="/list/new" element={<NewList />} />
+            <Route path="/lists/:listId/tasks/:taskId" element={<EditTask />} />
+            <Route path="/lists/:listId/edit" element={<EditList />} />
           </>
         ) : (
-          <Navigate to="/signin" />
+          <Route path="*" element={<Navigate to="/signin" />} />
         )}
-        <Route component={NotFound} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
