72c38a65955ef331fb196a3d87b8663ba1d3de76
yukinko05 <grey.sora.35@gmail.com>
2024-09-05 18:16:08 +0900

    APIにタスク期限をUTC基準でPOSTしてデータ取得時には日本のタイムゾーンに変換

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -4,7 +4,6 @@ export const convertToDisplayDate = (string) => {
 
 export const formatDateToISO = (string) => {
   const date = new Date(string);
-  date.setHours(date.getHours() + 9);
   const isoString = date.toISOString();
   const formattedString = isoString.slice(0, 19) + 'Z';
   return formattedString;
@@ -12,7 +11,6 @@ export const formatDateToISO = (string) => {
 
 export const formatForDisplay = (isoDateString) => {
   const date = new Date(isoDateString);
-  date.setHours(date.getHours() - 9);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
