f9e3fa259eb0aec339ab6d8d962537c91c80ddf6
yukinko05 <grey.sora.35@gmail.com>
2024-09-11 18:38:44 +0900

    よく使用するStyleの値を変数で管理

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import './global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CookiesProvider } from 'react-cookie';
