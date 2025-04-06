da6a5cecbb678af7c07624ad9a489fd9d80ea83d
hina81 <i20203@e.hiroshima-cu.ac.jp>
2025-04-06 19:31:19 +0900

    react19にする

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { createRoot } from "react-dom/client";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -7,14 +8,13 @@ import { CookiesProvider } from "react-cookie";
 import { Provider } from "react-redux";
 import { store } from "./store"
 
-ReactDOM.render(
-  <Provider store={store}>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
-  </Provider>,
-  document.getElementById("root")
-);
+const root = createRoot(document.getElementById("root"));
+
+root.render(<Provider store={store}>
+  <CookiesProvider>
+    <App />
+  </CookiesProvider>
+</Provider>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
