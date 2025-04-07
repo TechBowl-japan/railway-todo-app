c89aad20ca46190d80557fc83d791ed3109d82bc
[roddyPiper54] <[hiroyuki.nakajima1887@gmail.com]>
2025-04-08 02:27:24 +0900

    eslint エラー解消

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,35 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
-import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser, // ← ブラウザ環境の変数をOKにする
+        ...globals.jest,
+        process: "readonly" // ← Node.js の process も読み取りだけOKにする
+      }
+    },
+    plugins: {
+      js,
+      react: pluginReact,
+    },
+    settings: {
+      react: {
+        version: "detect", //自動でReactバージョンを検出
+      },
+    },
+    rules: {
+      "react/prop-types": "off", //追記 eslint対策
+      // "no-unused-vars": "warn",
+      // "react/react-in-jsx-scope": "off",
+    },
+    extends: ["js/recommended"]
+  },
+
 ]);
