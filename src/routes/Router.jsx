0d802f2d865626faf61fdf623955588d14ef17da
vayaji <176355251+vayaji@users.noreply.github.com>
2025-09-19 11:36:21 +0900

    不必要になったものを削除

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,9 +4,7 @@ import { Sidebar } from "~/components/Sidebar"
 import NotFound from "~/pages/404"
 import Home from "~/pages/index.page"
 import NewList from "~/pages/list/new/index.page"
-import EditList from "~/pages/lists/[listId]/edit/index.page"
 import ListIndex from "~/pages/lists/[listId]/index.page"
-import EditTask from "~/pages/lists/[listId]/tasks/[taskId]/index.page"
 import SignIn from "~/pages/signin/index.page"
 import SignUp from "~/pages/signup/index.page"
 
@@ -25,11 +23,6 @@ export const Router = () => {
               <Route path="/" element={<Home />}></Route>
               <Route path="/lists/:listId" element={<ListIndex />}></Route>
               <Route path="/list/new" element={<NewList />}></Route>
-              <Route
-                path="/lists/:listId/tasks/:taskId"
-                element={<EditTask />}
-              ></Route>
-              <Route path="/lists/:listId/edit" element={<EditList />}></Route>
             </>
           ) : (
             <Route path="/" element={<Navigate replace to="/signin" />}></Route>
